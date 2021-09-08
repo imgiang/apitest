690236e3df9927d0bb479ba5fca0a5ad0118ab55
xuanlam <phamxuanlam@gmail.com>
2021-09-08 19:11:36 +0700

    MFLIX 7

--- src/main/java/service/TheaterService.java
+++ src/main/java/service/TheaterService.java
@@ -3,8 +3,6 @@ package service;
 import DAO.MongoDB.AbsDAO;
 import DAO.MongoDB.TheaterDAO;
 import com.mongodb.client.FindIterable;
-import lombok.Data;
-import model.Geo;
 import model.Theater;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,39 +16,10 @@ public class TheaterService extends AbsDAO {
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    @Path("/geojson")
-    public GeoJSON getGeoJSON() {
-        GeoJSON geoJson = new GeoJSON();
-        geoJson.setType("FeatureCollection");
-        geoJson.setFeatures(new ArrayList<>());
+    public ArrayList<Theater> getListTheater() {
         FindIterable<Theater> list = new TheaterDAO().getListTheater();
-        list.forEach(theater -> {
-            Feature feature = new Feature();
-            feature.setType("Feature");
-            feature.setGeometry(theater.getLocation().getGeo());
-            feature.setProperties(new Properties());
-            feature.getProperties().setAddress(theater.getLocation().getAddress().getStreet1() + ", " + theater.getLocation().getAddress().getCity());
-            geoJson.getFeatures().add(feature);
-        });
-        return geoJson;
+        return list.into(new ArrayList<>());
     }
-
 }
 
-@Data
-class GeoJSON {
-    private String type;
-    private ArrayList<Feature> features;
-}
-
-@Data
-class Feature {
-    private String type;
-    private Geo geometry;
-    private Properties properties;
-}
-@Data
-class Properties {
-    private String address;
-}
 
